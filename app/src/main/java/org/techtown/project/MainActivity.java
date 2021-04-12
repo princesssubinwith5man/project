438de7f0a7c369121aa39b52863615dbe172cf40
이삭 홍 <drm05003@kangwon.ac.kr>
2021-04-12 22:12:32 +0900

    반복문 추가했어요

--- app/src/main/java/org/techtown/project/MainActivity.java
+++ app/src/main/java/org/techtown/project/MainActivity.java
@@ -10,6 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // skfsdjlfjlksjdflsjdlfl
+
+
+        // for(didfjsi_
         setContentView(R.layout.activity_main);
     }
 }
