4d46c3d21cc1ff4cca806687e94d14254c2dfefe
YisakH <drm05003@naver.com>
2021-05-12 17:52:31 +0900

    스피너 구현 및 안드로이드 맵 띄우기 완료

---
+++ MyApplication7/app/src/main/java/org/techtown/myapplication/MainActivity2.java
@@ -0,0 +1,58 @@
+package org.techtown.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import android.content.Intent;
+
+
+public class MainActivity2 extends AppCompatActivity
+        implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private double lat;
+    private double lng;
+    private String lat1;
+    private String lng1;
+    private String cn;
+    private String fn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        Intent intent = getIntent();
+        lat1 = intent.getStringExtra("lat");
+        lng1 = intent.getStringExtra("lng");
+        cn = intent.getStringExtra("centername");
+        fn = intent.getStringExtra("fac");
+        //Log.d("tag:", "result: " + lat1 +" "+ lng1);
+        lat =Double.parseDouble(lat1);
+        lng =Double.parseDouble(lng1);
+
+    }
+    @Override
+    public void onMapReady(final GoogleMap googleMap) {
+        mMap = googleMap;
+        Log.d("tag:", "result: " + lat1 +" "+ lng1);
+        LatLng Corona = new LatLng(lat,lng);
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(Corona);
+        markerOptions.title(cn);
+        markerOptions.snippet(fn);
+        mMap.addMarker(markerOptions);
+        // 기존에 사용하던 다음 2줄은 문제가 있습니다.
+        // CameraUpdateFactory.zoomTo가 오동작하네요.
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
+        //mMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Corona, 14));
+    }
+}
