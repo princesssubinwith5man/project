413ca5560a84bbe8bb4f9bc24d4049dd42ed45d8
YisakH <drm05003@naver.com>
2021-05-17 18:03:33 +0900

    위치 버튼 추가는 했는데 왜 캘리포니아가 나오죠?

--- MyApplication7/app/src/main/java/org/techtown/myapplication/GMapActivity.java
+++ MyApplication7/app/src/main/java/org/techtown/myapplication/GMapActivity.java
@@ -1,6 +1,15 @@
 package org.techtown.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -10,10 +19,16 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import android.content.Intent;
+import android.webkit.PermissionRequest;
+import android.widget.Toast;
 
 
 public class GMapActivity extends AppCompatActivity
-        implements OnMapReadyCallback {
+        implements
+        OnMapReadyCallback,
+        GoogleMap.OnMyLocationButtonClickListener,
+        GoogleMap.OnMyLocationClickListener,
+        ActivityCompat.OnRequestPermissionsResultCallback {
 
     private double lat;
     private double lng;
@@ -21,6 +36,14 @@ public class GMapActivity extends AppCompatActivity
     private String lng1;
     private String cn;
     private String fn;
+
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+    String[] REQUIRED_PERMISSIONS  = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
+    private final int PERMISSIONS_REQUEST_CODE = 100;
+
+    private boolean permissionDenied = false;
+    private GoogleMap map;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,8 +61,15 @@ public class GMapActivity extends AppCompatActivity
         lng =Double.parseDouble(lng1);
 
     }
+    @SuppressLint("MissingPermission")
     @Override
     public void onMapReady(final GoogleMap googleMap) {
+
+        map = googleMap;
+
+        enableMyLocation();
+
+
         Log.d("tag:", "result: " + lat1 +" "+ lng1);
         LatLng Corona = new LatLng(lat,lng);
         MarkerOptions markerOptions = new MarkerOptions();
@@ -49,5 +79,36 @@ public class GMapActivity extends AppCompatActivity
         googleMap.addMarker(markerOptions);
 
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Corona, 14));
+
+    }
+
+    private void enableMyLocation() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            if (map != null) {
+                map.setMyLocationEnabled(true);
+                map.setOnMyLocationButtonClickListener(this);
+                map.setOnMyLocationClickListener(this);
+            }
+        } else {
+            // Permission to access the location is missing. Show rationale and request permission
+            ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS,
+                    PERMISSIONS_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onMyLocationClick(@NonNull Location location) {
+        Toast.makeText(this, "Current location:\n" + location, Toast.LENGTH_LONG)
+                .show();
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT)
+                .show();
+        // Return false so that we don't consume the event and the default behavior still occurs
+        // (the camera animates to the user's current position).
+        return false;
     }
 }
